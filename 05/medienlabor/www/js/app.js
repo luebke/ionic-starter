d1d8817a64b0e5f683902bd599c05a3fde2cf66a
marco <moshking666@gmail.com>
2016-03-05 00:06:58 +0100

    medienlabor app

--- 05/medienlabor/www/js/app.js
+++ 05/medienlabor/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('medienlaborapp', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -21,4 +21,12 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
+})
+
+.controller("ListCtrl", function( $scope, $http ){
+
+  $http.get("js/data.json").then(function(data){
+    $scope.data = data.data;
+  });
+
 })
